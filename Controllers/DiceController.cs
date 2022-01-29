3fa27f7ebe087619cf26fa01a68382263a0b72f0
Chad <chadlada@gmail.com>
2022-01-29 17:13:20 -0500

    Added dice controller

---
+++ Controllers/DiceController.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BasicAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DiceController : ControllerBase
+    {
+        [HttpGet]
+        public int Roll(int sides)
+        {
+            return 1;
+        }
+    }
+}
